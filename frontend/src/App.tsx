ca7c700aaa1e7c046ca27879e33544f9e2e941e5
Eric Daniels <e.danielshuaman@lancaster.ac.uk>
2024-12-23 16:10:02 +0000

    Fix header styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is
-          {" "}
-          {count}
-        </button>
-        <p>
-          Edit
-          {" "}
-          <code>src/App.tsx</code>
-          {" "}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Real time stock</h1>
+      <h2>Stocks 2</h2>
+      <h3>Stocks 3</h3>
+      <h4>Stocks 4</h4>
+      <h5>Stocks 5</h5>
+      <h6>Stocks 6</h6>
+      <p>Stocks par</p>
+      <small>Stocks small</small>
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Include all files with Tailwind classes
+    "./index.html", // Include index.html in the root directory
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in the src folder
   ],
   theme: {
-    extend: {}, // Add customizations here
+    extend: {}, // Add your custom styles here
   },
-  plugins: [], // Add plugins here if needed
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ], // Add Tailwind plugins if needed
 };
